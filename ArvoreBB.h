78e1609f3f42320e564653d6aa0dababfa02ddee
Vitor Campos <vlfc.jogos@gmail.com>
2023-07-14 19:49:44 -0300

    Biblioteca de Árvore Binária

--- ArvoreBB.h
+++ ArvoreBB.h
@@ -186,4 +186,99 @@ void destroi_arvore(tp_arvore *raiz){
 	//free(*raiz);
 	*raiz=NULL;
 }
+
+	typedef struct tp_fila {
+	tp_no *no;
+	struct tp_fila *prox;
+	} tp_fila;
+	tp_fila * inicia_fila() {
+	return NULL;
+	}
+	int fila_vazia(tp_fila **fila) {
+	if(*fila == NULL) return 1;
+	return 0;
+	}
+	tp_fila * aloca_fila(tp_no *no) {
+	tp_fila *no_fila = (tp_fila*) malloc(sizeof(tp_fila));
+	no_fila->no = no;
+	no_fila->prox = NULL;
+	return no_fila;
+	}
+	int insere_fila(tp_fila **fila, tp_no *no) {
+	tp_fila *no_fila = aloca_fila(no), *atu = *fila;
+	if(no_fila == NULL) return 0;
+	if(fila_vazia(fila)) *fila = no_fila;
+	else {
+	while(atu->prox != NULL) {
+	atu = atu->prox;
+	}
+	atu->prox = no_fila;
+	}
+	return 1;
+	}
+	tp_no * remove_fila(tp_fila **fila) {
+	tp_fila *atu = *fila;
+	*fila = atu->prox;
+	tp_no *no = atu->no;
+	free(atu);
+	atu = NULL;
+	return no;
+	}
+	void imprime_fila(tp_fila **fila) {
+	tp_fila *atu = *fila;
+	while(atu != NULL) {
+	printf("%d ", atu->no->info);
+	atu = atu->prox;
+	}
+	printf("\n");
+	}
+	void destroi_fila(tp_fila **fila) {
+	tp_fila *atu = *fila;
+	while(atu != NULL) {
+	*fila = atu->prox;
+	free(atu);
+	atu = *fila;
+	}
+	*fila = NULL;
+	}
+	void imprime_BFS(tp_arvore *raiz) {
+	if(!arvore_vazia(*raiz)) {
+	tp_fila *fila = inicia_fila();
+	insere_fila(&fila, *raiz);
+	while(!fila_vazia(&fila)) {
+	tp_no *no = remove_fila(&fila);
+	cout<<no->info;
+	if(no->esq != NULL) insere_fila(&fila, no->esq);
+	if(no->dir != NULL) insere_fila(&fila, no->dir);
+	}
+	destroi_fila(&fila);
+	}
+	}
+	
+	int indprefixa;
+	
+	int busca_infixa(char *infixa, int esq, int dir, tp_item info){
+	int i;
+	for (i = esq; i <= dir; i++)
+	if (infixa[i] == info)
+	return i;
+	return -1;
+}
+
+tp_arvore monta_arvore(char prefixa[], char infixa[], int esq, int dir){
+	if (esq > dir)
+	return NULL;
+	//Aloca memória para um n , preenche com o n raiz? ? ?
+	tp_no *no = aloca_no();
+	no->info = prefixa[indprefixa];
+	no->dir = no->esq = NULL;
+	indprefixa++;
+	if (esq == dir) // Se n folha, retorna o end. do n para a chamada anterior? ?
+	return no;
+	int indinfixa = busca_infixa(infixa, esq, dir, no->info);
+	no->esq = monta_arvore(prefixa, infixa, esq, indinfixa-1);
+	no->dir = monta_arvore(prefixa, infixa, indinfixa+1, dir);
+	return no;
+}
+
 #endif
